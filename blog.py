c120814fbf585022d6d74b19a5fcc37c1636d5a9
gabatrone11 <gabatrone11@gmail.com>
2015-11-20 10:06:28 -0800

    commit 11/20/2015

--- blog.py
+++ blog.py
@@ -1,7 +1,7 @@
 # blog.py - controller
 
 #imports
-from flask import Flask, render_template, requests, session, \
+from flask import Flask, render_template, request, session, \
     flash, redirect, url_for, g
 import sqlite3
 
@@ -16,6 +16,15 @@ app.config.from_object(__name__)
 # function used for connecting to the database
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
